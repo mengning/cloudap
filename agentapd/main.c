b24de3b51135a884acf522c433537b4b5c6eefc1
mengning <mengning@ustc.edu.cn>
2014-01-08 11:24:36 +0800

    为移入socket client做一些准备梳理工作

--- agentapd/main.c
+++ agentapd/main.c
@@ -1,4 +1,12 @@
-
+/*
+ * Agentapd interaction with socket to remoteapd(driver_nl80211ext)
+ * Copyright (c) 2013-2014, SSE@USTCSZ mengning <mengning@ustc.edu.cn>
+ *
+ * agentapd(socket client)with Linux nl80211/cfg80211 - driver_nl80211ext.c(socket server)
+ *
+ * This software may be distributed under the terms of the BSD license.
+ * See README for more details.
+ */
 #include<stdio.h> 			/* perror */
 #include<stdlib.h>			/* exit	*/
 #include<sys/types.h>		/* WNOHANG */
@@ -9,7 +17,7 @@
 
 struct hostapd_data
 {
-    int data;
+    void * drv_priv;
 };
 
 extern struct wpa_driver_ops *wpa_drivers[];
@@ -22,28 +30,36 @@ extern struct wpa_driver_ops *wpa_drivers[];
 int main() 
 {
     int i = 0;
+    void * global_priv;
+    struct hostapd_data hapd;
+    struct wpa_init_params params;
+    /* init socket client */
+    
+    /* init nl80211 */ 
 	for (i = 0; wpa_drivers[i]; i++) 
 	{
 		if (wpa_drivers[i]->global_init) 
 		{
-			void * p = wpa_drivers[i]->global_init();
-			if (p == NULL) {
+			global_priv = wpa_drivers[i]->global_init();
+			if (global_priv == NULL) {
 				printf("global_init Failed to initialize\n");
 				return -1;
 			}
 		}
-		struct hostapd_data hapd;
-		struct wpa_init_params params;
+        /* recv params from remote */
+        
+        params.global_priv = global_priv;
 		if (wpa_drivers[i]->hapd_init) 
 		{
-			void * p = wpa_drivers[i]->hapd_init(&hapd,&params);
-			if (p == NULL) {
+			hapd.drv_priv = wpa_drivers[i]->hapd_init(&hapd,&params);
+			if (hapd.drv_priv == NULL) {
 				printf("hapd_init Failed to initialize\n");
 				return -1;
 			}		    
 		}		    
 	}
 	printf("NL80211 initialized\n");
+	eloop_run();
     return 0;
 }
 
